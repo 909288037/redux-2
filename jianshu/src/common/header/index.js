e9276a5ee19049bb3aee7338d7332a602db75b40
liushuai <909288037@qq.com>
2018-09-26 22:56:00 +0800

    头部导航样式

--- jianshu/src/common/header/index.js
+++ jianshu/src/common/header/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import {HeaderWrapper, Logo, Nav} from './style'
+import {HeaderWrapper, Logo, Nav, NavItem, NavSearch, Addition, Button} from './style'
 
 class Header extends Component {
     render() {
         return (
             <HeaderWrapper>
                 <Logo href="/"></Logo>
-                <Nav></Nav>
+                <Nav>
+                    <NavItem className="left active">首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登录</NavItem>
+                    <NavItem className="right">Aa</NavItem>
+                    <NavSearch placeholder="搜索"></NavSearch>
+                </Nav>
+                <Addition>
+                    <Button className="writing">写文章</Button>
+                    <Button className="sign-up">注册</Button>
+                </Addition>
+                
             </HeaderWrapper>
         )
     }
--- jianshu/src/common/header/style.js
+++ jianshu/src/common/header/style.js
@@ -24,5 +24,72 @@ export const Nav = styled.div`
     width: 960px;
     height: 100%;
     margin: 0 auto;
-    background: pink
+    padding-right: 50px;
+    background: white;
+    box-sizing: border-box
+`
+
+export const NavItem = styled.div`
+    line-height: 56px;
+    padding: 0 15px;
+    color: #333;
+    font-size: 17px;
+    font-family: -apple-system,SF UI Text,Arial,PingFang SC,Hiragino Sans GB,Microsoft YaHei,WenQuanYi Micro Hei,sans-serif;
+    &.left {
+        float: left;
+    }
+    &.right {
+        float: right;
+        color: #969696;
+    }
+    &.active {
+        color: #ea6f5a
+    }
+`
+
+export const NavSearch = styled.input`
+    width: 160px;
+    height: 38px;
+    margin-top: 9px;
+    margin-left: 15px;
+    padding: 0 40px 0 20px;
+    border: none;
+    outline: none;
+    border-radius: 19px;
+    background: #eee;
+    box-sizing: border-box;
+    font-size: 14px;
+    &::placeholder {
+        color: #999
+    }
+`
+
+export const Addition = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 56px;
+`
+export const Button = styled.div`
+    float: right;
+    border: 1px solid #ec6149b3;
+    border-radius: 20px;
+    font-size: 15px;
+    line-height: 38px;
+    padding: 0 12px;
+    margin-top: 9px;
+    margin-right: 20px;
+    &.sign-up {
+        width: 80px;
+        text-align: center;
+        color: #ea6f5a;
+        box-sizing: border-box
+    }
+    &.writing {
+        width: 100px;
+        box-sizing: border-box;
+        background: #ea6f5a;
+        text-align: center;
+        color: #fff
+    }
 `
